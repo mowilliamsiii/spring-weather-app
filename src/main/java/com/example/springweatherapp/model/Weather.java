acbd33ba5ce21b106ec5f374e36979e0657bffee
mowilliamsiii <moseswilliamsiii99@gmail.com>
2022-07-03 14:16:36 -0400

    created model

--- src/main/java/com/example/springweatherapp/model/Weather.java
+++ src/main/java/com/example/springweatherapp/model/Weather.java
@@ -1,4 +1,44 @@
 package com.example.springweatherapp.model;
 
+import java.math.BigDecimal;
+
 public class Weather {
+    private String description;
+    private BigDecimal temperature;
+    private BigDecimal feelsLike;
+    private BigDecimal windSpeed;
+
+    public Weather(String description, BigDecimal temperature, BigDecimal feelsLike, BigDecimal windSpeed) {
+        this.description = description;
+        this.temperature = temperature;
+        this.feelsLike = feelsLike;
+        this.windSpeed = windSpeed;
+    }
+
+    public BigDecimal getTemperature() {
+        return temperature;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+
+    public BigDecimal getFeelsLike() {
+        return feelsLike;
+    }
+
+    public BigDecimal getWindSpeed() {
+        return windSpeed;
+    }
+
+    @Override
+    public String toString() {
+        return "Weather{" +
+                "description='" + description + '\'' +
+                ", temperature='" + temperature + '\'' +
+                ", feelsLike=" + feelsLike +
+                ", windSpeed=" + windSpeed +
+                '}';
+    }
 }
