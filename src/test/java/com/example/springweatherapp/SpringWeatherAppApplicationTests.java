e6de058f35a88d245a7dc809b0e6f473e57ac194
mowilliamsiii <moseswilliamsiii99@gmail.com>
2022-07-03 13:36:05 -0400

    First Add

---
+++ src/test/java/com/example/springweatherapp/SpringWeatherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springweatherapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWeatherAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
